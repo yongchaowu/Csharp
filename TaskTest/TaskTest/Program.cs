f19d7b79142f0bf8671a7c4faa636424e8bcbd54
yongchaowu <wyclswq@163.com>
2019-01-30 16:22:27 +0800

    20190130
    C# Thread ThreadPool
    c# Task

---
+++ TaskTest/TaskTest/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTest
+{
+    class Program
+    {
+        static void TaskMehtod()
+        {
+            Console.WriteLine("runnint in a task");
+            Console.WriteLine("Task id: {0}",Task.CurrentId);
+        }
+        static void TaskMehtod2(Task t)
+        {
+            Console.WriteLine("task {0} finished",t.Id);
+            Console.WriteLine("Task id: {0}", Task.CurrentId);
+        }
+        static void Main(string[] args)
+        {
+            //using task factory
+            TaskFactory tf = new TaskFactory();
+            Task tl = tf.StartNew(TaskMehtod);
+
+            //using the task factory via a task
+            Task t2 = Task.Factory.StartNew(TaskMehtod);
+
+            // using Task constructor
+            Task t3 = new Task(TaskMehtod);
+            t3.Start();
+            //TaskCreationOptions
+            Task t4 = new Task(TaskMehtod, TaskCreationOptions.PreferFairness);
+            t4.Start();
+            // ContinueWith  连续任务
+            Task t5 = t4.ContinueWith(TaskMehtod2);
+            // TaskContinuationOptions 指定连续任务启动条件 
+            Task t6 = t5.ContinueWith(TaskMehtod2, TaskContinuationOptions.OnlyOnRanToCompletion);
+            ;
+
+
+            // 层次结构  在Task中 new一个Task
+
+            // 任务的结果 泛型类Task<TResult>   Task实例的Result属性
+            /*
+             任务结束时,它可以把一些有用的状态信息写到共享对象中。这个共享对象必须是线程安全的。
+             另一个选项是使用返回某个结果的任务。使用Task类的泛型版本,就可以定义返回某个结果的任务的返回类型
+             */
+
+            while (true)
+            {
+                ;
+            }
+        }
+    }
+}
---
+++ Thread/Thread/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ThreadTest
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    var t1 = new Thread(ThreadMain);
+        //    t1.Start();
+        //    Console.WriteLine("This is the main thread.");
+        //    int time = 10;
+        //    while (time-- != 0)
+        //    {
+        //        Console.WriteLine("*");
+        //    }
+        //}
+
+        //static void ThreadMain()
+        //{
+        //    Console.WriteLine("Running in a thread");
+        //    int time = 10;
+        //    while (time-- != 0)
+        //    {
+        //        Console.WriteLine(".");
+        //    }
+        //}
+
+        //----------------------------------------------------------------
+        //static void Main(string[] args)
+        //{
+        //    var t1 = new Thread(()=> {
+        //        Console.WriteLine("running in a thread, id:{0}", Thread.CurrentThread.ManagedThreadId);
+        //        int time = 10;
+        //        while (time-- != 0)
+        //        {
+        //            Console.WriteLine(".");
+        //        }
+        //    });
+        //    t1.Start();
+        //    Console.WriteLine("This is the main thread. id :{0}",Thread.CurrentThread.ManagedThreadId);
+        //    int time2 = 10;
+        //    while (time2-- != 0)
+        //    {
+        //        Console.WriteLine("*");
+        //    }
+        //}
+
+        //------------------------------------------------------------------------------------------------------------
+        // ThreadPool [1/30/2019 yongchao]
+        static void Main(string[] args)
+        {
+            int nWorkerThreads;
+            int nCompletionPortThreads;
+            ThreadPool.GetMaxThreads(out nWorkerThreads, out nCompletionPortThreads);
+            Console.WriteLine("Max worker threads: {0}," +
+                "I/O completion threads: {1}",
+                nWorkerThreads, nCompletionPortThreads);
+
+            for (int i = 0; i < 5;i++ )
+            {
+                ThreadPool.QueueUserWorkItem(JobForAThread);
+            }
+            Thread.Sleep(3000);
+        }
+
+        static void JobForAThread(object state)
+        {
+            for (int i = 0; i < 3;i++ )
+            {
+                Console.WriteLine("loop {0},running inside pooled thread {1}",
+                    i, Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(50);
+            }
+        }
+
+
+
+    }
+}
